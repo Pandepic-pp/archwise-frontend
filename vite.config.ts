8cd089f0d3fa1cd7374e3745cc5073630a6c6682
pratyakshpandey <pratyaksh.pandey@worldfashionexchange.com>
2026-04-29 16:17:38 +0530

    push

--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://archwise-backend.onrender.com/',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://archwise-backend.onrender.com/',
         changeOrigin: true,
         ws: true,
       },
